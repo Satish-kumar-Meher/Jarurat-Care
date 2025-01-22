dd55d8410ec4550ff351105d2ccb42ec35358421
Satish-kumar-Meher <162144375+Satish-kumar-Meher@users.noreply.github.com>
2025-01-22 16:26:54 +0530

    Update db.js

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -8,13 +8,7 @@ const URI = process.env.MONGODB_URI;
 const connectDB = async () => {
   try {
     // Add options to optimize connection
-    const connection = await mongoose.connect(URI, {
-      useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000, // Increase to 30 seconds
-  socketTimeoutMS: 45000, // Increase to 45 seconds
-  maxPoolSize: 10, // Limit connection pool size
-    });
+    const connection = await mongoose.connect(URI);
 
     console.log(`Connected to MongoDB: ${connection.connection.host}`);
   } catch (error) {
